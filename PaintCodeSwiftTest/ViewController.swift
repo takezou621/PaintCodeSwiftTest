0dbd13374a346dc44448e237f5d08574718c3047
Takeshi Kawai <takezou621@gmail.com>
2014-10-03 18:37:56 +0900

    first commit

---
+++ PaintCodeSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaintCodeSwiftTest
+//
+//  Created by KawaiTakeshi on 2014/10/03.
+//  Copyright (c) 2014年 KawaiTakeshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
